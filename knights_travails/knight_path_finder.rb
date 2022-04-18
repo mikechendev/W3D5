5cac74b1ab31c335e97a748751fdf8de321437be
Michael Chen <michael.chen.1120@gmail.com>
2022-04-18 10:55:22 -0400

    Fix knight path finder

--- knights_travails/knight_path_finder.rb
+++ knights_travails/knight_path_finder.rb
@@ -13,8 +13,15 @@ class KnightPathFinder
         #arr
         current_x, current_y = pos[0], pos[1]
         arr = []
-        (p1 + 2 && p2 + 1) (p1 + 1 && p2 + 2) && (p1 + 2 && p2 - 1) || (p1 + 1 && p2 - 2)
-        || (p1 - 1 && p2 - 2) || (p1 - 2 && p2 - 1) || (p1 - 2 && p2 + 1) || (p1 -1 && p2 + 2)
+        moves = [[2,1],[1,2],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1],[-1,2]]
+        moves.each do |move|
+            new_pos = []
+            new_pos << move[0] + current_x
+            new_pos << move[1] + current_y
+            arr << new_pos
+        end
+        arr.reject { |pos| pos[0] > 7 || pos[0] < 0 || pos[1] > 7 || pos[1] < 0 }
+        arr.map { |pos| KnightPathFinder.new(pos) }
     end
 
     def new_move_positions(pos)
@@ -24,6 +31,13 @@ class KnightPathFinder
 
     def build_move_tree
         new_move_positions(@root_node)
-        #use polytreenode
+        queue = [@root_node]
+        queue << new_move_positions(@root_node)
+        until queue.empty?
+            el = queue.shift
+            return el if el.value == target
+            el.children.each { |child| queue << child}
+        end
+        nil
     end
 end
