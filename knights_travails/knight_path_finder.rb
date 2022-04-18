83a5397224ca3838c732ae90d63f2dd4d050a461
Tom Li <tomleslieli@yahoo.com>
2022-04-18 13:38:38 -0400

    Up to trace_path_back

--- knights_travails/knight_path_finder.rb
+++ knights_travails/knight_path_finder.rb
@@ -1,15 +1,17 @@
 require_relative 'polytreenode'
 
 class KnightPathFinder
-
+    attr_reader :set
     BOARD = Array.new(8) {Array.new(8)}
+    
     def initialize(pos)
         @root_node = PolyTreeNode.new(pos)
         @considered_positions = [pos]
+        @set = Set.new()
     end
 
     def self.valid_moves(pos)
-        #arr
+        current_node = PolyTreeNode.new(pos)
         current_x, current_y = pos[0], pos[1]
         arr = []
         moves = [[2,1],[1,2],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1],[-1,2]]
@@ -18,6 +20,10 @@ class KnightPathFinder
         end
         arr.reject! { |pos| pos[0] > 7 || pos[0] < 0 || pos[1] > 7 || pos[1] < 0 }
         arr.map! { |pos| PolyTreeNode.new(pos) }
+        arr.each do |ele|
+            current_node.children << ele
+            ele.parent = current_node
+        end
     end
 
     def new_move_positions(pos)
@@ -27,6 +33,7 @@ class KnightPathFinder
     end
 
     def build_move_tree(start, target)
+
         new_move_positions(start)
         queue = [start]
         queue << new_move_positions(start)
@@ -36,5 +43,12 @@ class KnightPathFinder
             el.children.each { |child| queue << child}
         end
         nil
+    end
+
+    def trace_path_back
+        return = []
+
+        
+
     end
 end
