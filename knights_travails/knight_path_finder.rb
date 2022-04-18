c89e0cf379e77cbbf0be32db814290bedcfaa89d
Michael Chen <michael.chen.1120@gmail.com>
2022-04-18 12:03:31 -0400

    Fix valid moves

--- knights_travails/knight_path_finder.rb
+++ knights_travails/knight_path_finder.rb
@@ -6,7 +6,6 @@ class KnightPathFinder
     def initialize(pos)
         @root_node = PolyTreeNode.new(pos)
         @considered_positions = [pos]
-        build_move_tree
     end
 
     def self.valid_moves(pos)
@@ -15,24 +14,22 @@ class KnightPathFinder
         arr = []
         moves = [[2,1],[1,2],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1],[-1,2]]
         moves.each do |move|
-            new_pos = []
-            new_pos << move[0] + current_x
-            new_pos << move[1] + current_y
-            arr << new_pos
+            arr << [move[0] + current_x, move[1] + current_y]
         end
-        arr.reject { |pos| pos[0] > 7 || pos[0] < 0 || pos[1] > 7 || pos[1] < 0 }
-        arr.map { |pos| KnightPathFinder.new(pos) }
+        arr.reject! { |pos| pos[0] > 7 || pos[0] < 0 || pos[1] > 7 || pos[1] < 0 }
+        arr.map! { |pos| PolyTreeNode.new(pos) }
     end
 
     def new_move_positions(pos)
         remaining_pos = KnightPathFinder.valid_moves(pos).reject { |move| @considered_positions.include?(move) }
         @considered_positions += remaining_pos
+        remaining_pos
     end
 
-    def build_move_tree
-        new_move_positions(@root_node)
-        queue = [@root_node]
-        queue << new_move_positions(@root_node)
+    def build_move_tree(start, target)
+        new_move_positions(start)
+        queue = [start]
+        queue << new_move_positions(start)
         until queue.empty?
             el = queue.shift
             return el if el.value == target
